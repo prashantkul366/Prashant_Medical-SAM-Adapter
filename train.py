5189d68d898b76c114463ddaa0603825546ad755
Prashant Kulkarni <prashantkul366@gmail.com>
2026-01-23 17:30:43 +0530

    BUSI

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ from dataset import *
 from utils import *
 
 def main():
-
+    print("=========> Loading config <========= ")
     args = cfg.parse_args()
 
     seed = args.seed
@@ -103,8 +103,8 @@ def main():
     max_epochs = 1000
 
 
-    for epoch in range(settings.EPOCH):
-
+    for epoch in range(max_epochs):
+        print(f'\nEpoch: {epoch}/{max_epochs} || Learning Rate: {scheduler.get_last_lr()[0]}')
         if epoch < 5:
             if args.dataset != 'REFUGE':
                 tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
@@ -193,74 +193,6 @@ def main():
                     f"Best Dice: {best_dice:.4f} (epoch {best_epoch})"
                 )
                 break
-
-        # net.eval()
-        # if epoch and epoch % args.val_freq == 0 or epoch == settings.EPOCH-1:
-        #     if args.dataset != 'REFUGE':
-        #         tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-        #         logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
-        #     else:
-        #         tol, (eiou_cup, eiou_disc, edice_cup, edice_disc) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-        #         logger.info(f'Total score: {tol}, IOU_CUP: {eiou_cup}, IOU_DISC: {eiou_disc}, DICE_CUP: {edice_cup}, DICE_DISC: {edice_disc} || @ epoch {epoch}.')
-
-        #     if args.distributed != 'none':
-        #         sd = net.module.state_dict()
-        #     else:
-        #         sd = net.state_dict()
-
-        #     # if edice > best_dice:
-        #     #     best_tol = tol
-        #     #     is_best = True
-
-        #     #     save_checkpoint({
-        #     #     'epoch': epoch + 1,
-        #     #     'model': args.net,
-        #     #     'state_dict': sd,
-        #     #     'optimizer': optimizer.state_dict(),
-        #     #     'best_tol': best_dice,
-        #     #     'path_helper': args.path_helper,
-        #     # }, is_best, args.path_helper['ckpt_path'], filename="best_dice_checkpoint.pth")
-        #     # else:
-        #     #     is_best = False
-
-        #     if edice > best_dice:
-        #         logger.info(
-        #             f" Saving best model: Dice improved "
-        #             f"{best_dice:.4f} → {edice:.4f}"
-        #         )
-
-        #         best_dice = edice
-        #         best_epoch = epoch
-
-        #         save_checkpoint({
-        #             'epoch': epoch,
-        #             'model': args.net,
-        #             'state_dict': sd,
-        #             'optimizer': optimizer.state_dict(),
-        #             'best_dice': best_dice,
-        #             'path_helper': args.path_helper,
-        #         }, is_best=True,
-        #         ckpt_path=args.path_helper['ckpt_path'],
-        #         filename="best_dice_checkpoint.pth")
-
-        #     else:
-        #         logger.info(
-        #             f" Dice did not improve. Best: {best_dice:.4f} "
-        #             f" @ epoch {best_epoch}"
-        #         )
-
-        #     early_stop_count = epoch - best_epoch
-
-        #     logger.info(
-        #         f" Early stopping counter: "
-        #         f"{early_stop_count}/{early_stop_patience}"
-        #     )
-
-        #     if early_stop_count >= early_stop_patience:
-        #         logger.info(" Early stopping triggered. Training stopped.")
-        #         break
-
-                    
 
 
     writer.close()
